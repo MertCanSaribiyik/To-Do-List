adbd5efad08d520ac31aaa0a2488771e6e160b1c
Mert Can Sarıbıyık <103216730+MertCanSaribiyik@users.noreply.github.com>
2025-02-02 19:58:00 +0300

    Made code improvements and fixed task completion bug.

--- script.js
+++ script.js
@@ -1,136 +1,105 @@
 const inputTxt = document.querySelector(".input-area input");
-const createTaskBtn = document.querySelector(".input-area button");
+const createBtn = document.querySelector(".input-area button");
 const list = document.querySelector(".list");
 
 const maxtaskItemCount = 10;
-let id;
 
-//Setting the placeholder : 
+let tasks = [];
+
+//Setting the placeholder :
 inputTxt.placeholder = `Enter a task (max ${maxtaskItemCount})`;
 
-if(localStorage.getItem("nothingTask") === null) 
-    localStorage.setItem("nothingTask", 1);
+//Runs when the page load :
+window.onload = () => {
+  //nothing task :
+  if (!localStorage.getItem("visited")) {
+    createTaskInLocalStorage("nothing 🤠");
+    localStorage.setItem("visited", true);
+  }
 
+  tasks = JSON.parse(localStorage.getItem("tasks"));
 
-//Runs when the page load :  
-window.onload = function() {
-    
-    if(localStorage.getItem("nothingTask") == 1) {
-        createTask("Nothing 🤠", 0);
-    }        
+  tasks.forEach((item) => {
+    createTaskInUI(item.taskName, item.isCompeleted);
+  });
+};
 
-    if(localStorage.getItem("id") === null) {
-        id = 1;
-        localStorage.setItem("id", 1);
-    }
+createBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const taskName = inputTxt.value;
+  if (!taskName) return;
 
-    else 
-        id = parseInt(localStorage.getItem("id"));
+  if (tasks.length < maxtaskItemCount) {
+    createTaskInLocalStorage(taskName);
+    createTaskInUI(taskName, false);
+  }
 
-
-    for(let i = 1; i <= id; i++) {
-        if(localStorage.getItem(`task-${i}`) !== null)
-            createTask(localStorage.getItem(`task-${i}`), i);
-    }
-}
-
-
-//When the plus button is clicked : 
-createTaskBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    let taskTxt = inputTxt.value.charAt(0).toUpperCase() + inputTxt.value.slice(1, inputTxt.value.lenght);
-    inputTxt.value = "";
-
-    if (!controlInputTxt(taskTxt)) {
-        return;
-    }
-
-    if (controlTaskItemCount()) {
-        createTask(taskTxt, id);
-        localStorage.setItem(`task-${id++}`, taskTxt);
-        localStorage.setItem("id", id); 
-    }
-
+  inputTxt.value = "";
 });
 
-//Adding event listeners to ul :
 list.addEventListener("click", (e) => {
-    //when the check button is clicked :
-    if (e.target.classList.contains("fa-check")) {
-        const listItem = e.target.parentElement.parentElement.parentElement;
-        listItem.classList.toggle("text-decoration");
-        listItem.classList.toggle("opacity");
-    }
+  //when the check button is clicked :
+  if (e.target.classList.contains("fa-check")) {
+    const listItem = e.target.parentElement.parentElement.parentElement;
+    const taskName = listItem.innerText;
 
-    //When the trash button is clicked : 
-    else if (e.target.classList.contains("fa-trash")) {
-        const listItem = e.target.parentElement.parentElement.parentElement;
-        const listItemId = listItem.getAttribute("id");
+    taskCompletionInLocalStorage(taskName);
+    taskCompletionInUI(listItem);
+  }
 
-        if(listItemId === "task-0") {
-            console.log(listItemId);
-            localStorage.setItem("nothingTask", 0);
-        } 
+  //When the trash button is clicked :
+  else if (e.target.classList.contains("fa-trash")) {
+    const listItem = e.target.parentElement.parentElement.parentElement;
+    const taskName = listItem.innerText;
 
-        localStorage.removeItem(listItemId);
-        listItem.remove();
-        isThereTask();
-    }
+    removeTaskInLocalStorage(taskName);
+    listItem.remove();
+  }
 });
 
-//We created new task (li) : 
-function createTask(taskTxt, id) {
-    const li = document.createElement("li");
-    li.classList.add("list-item");
-    li.setAttribute("id", `task-${id}`);
-    li.textContent = taskTxt;
-    list.appendChild(li);
+//Create Tasks Functions :
+const createTaskInLocalStorage = (taskName) => {
+  tasks.push({ taskName, isCompeleted: false });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
 
-    const div = document.createElement("div");
-    div.classList.add("buttons");
-    div.innerHTML = `<button><i class="fa-solid fa-check"></i></button>
+const createTaskInUI = (taskName, isCompeleted) => {
+  const li = document.createElement("li");
+  li.classList.add("list-item");
+  if (isCompeleted) addingTaksCompletionClass(li);
+  li.textContent = taskName;
+  list.appendChild(li);
+
+  const div = document.createElement("div");
+  div.classList.add("buttons");
+  div.innerHTML = `<button><i class="fa-solid fa-check"></i></button>
                      <button><i class="fa-solid fa-trash"></i></button>`;
-    li.appendChild(div);
-}
+  li.appendChild(div);
+};
 
-
-//We checking to enter the correct text in the input : 
-function controlInputTxt(txt) {
-    let spaceCount = 0;
-
-    //Find the number of spaces in a variable : 
-    for (let i = 0; i < txt.length; i++) {
-        if (txt.charAt(i) === " ") 
-            spaceCount++;
+//Complete Tasks Functions :
+const taskCompletionInLocalStorage = (taskName) => {
+  tasks.map((item) => {
+    if (item.taskName === taskName) {
+      item.isCompeleted = !item.isCompeleted;
     }
+  });
 
-    if (txt === "")
-        return false;
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
 
-    else if (txt.length === spaceCount) {
-        return false;
-    }
+const taskCompletionInUI = (listItem) => {
+  listItem.classList.toggle("text-decoration");
+  listItem.classList.toggle("opacity");
+};
 
-    return true;
-}
+const addingTaksCompletionClass = (listItem) => {
+  listItem.classList.add("text-decoration");
+  listItem.classList.add("opacity");
+};
 
-//We check if the number of tasks (li) has reached the maximum value : 
-function controlTaskItemCount() {
-    const taskItems = document.querySelectorAll(".list .list-item");    //li
-
-    if (taskItems.length < maxtaskItemCount)
-        return true
-
-    return false;
-}
-
-//If there is no task (li), id is reset : 
-function isThereTask() {
-    const taskItems = document.querySelectorAll(".list .list-item");    //li
-
-    if(taskItems.length === 0) {
-        localStorage.setItem("id", 1);
-        id = 1;
-    }
-}
+//Remove tasks functions :
+const removeTaskInLocalStorage = (taskName) => {
+  tasks = tasks.filter((item) => item.taskName !== taskName);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
